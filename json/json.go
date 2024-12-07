6c9deedbf41b494a1c8e87f3b6aa778c649d7220
yangshijunxxx <yangshijunxxx@gmail.com>
2024-12-08 02:06:26 +0800

    feat: json序列化

--- json/json.go
+++ json/json.go
@@ -8,12 +8,29 @@ import (
 type User struct {
 	Name string `json:"name"`
 	Age  int    `json:"age"`
+	// phone 不序列化
+	Phone string `json:"-"`
+	// email 值为空时不序列化
+	Email string `json:"email,omitempty"`
+	// 不加json 标签，默认平铺
+	Address Address `json:"address"`
+}
+
+type Address struct {
+	Street string `json:"street"`
+	City   string `json:"city"`
 }
 
 func main() {
 	user := User{
-		Name: "John",
-		Age:  30,
+		Name:  "John",
+		Age:   30,
+		Phone: "123456789",
+		Email: "",
+		Address: Address{
+			Street: "123 Main St",
+			City:   "Anytown",
+		},
 	}
 	fmt.Print(user)
 	//  打印序列化的结果
