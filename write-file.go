e920deef72791978d635c1a6a1ceb4ac661818c6
yangshijunxxx <yangshijunxxx@gmail.com>
2024-12-22 18:57:28 +0800

    feat: 写文件使用缓存优化10倍速度

---
+++ write-file.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+func writeWithoutBuffer(filename string) error {
+	// 创建文件
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// 写入10000行数据
+	for i := 0; i < 10000; i++ {
+		_, err := file.WriteString(fmt.Sprintf("This is line %d\n", i))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func writeWithBuffer(filename string) error {
+	// 创建文件
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// 使用bufio.Writer来写入文件
+	writer := bufio.NewWriter(file)
+	// 写入10000行数据
+	for i := 0; i < 10000; i++ {
+		_, err := writer.WriteString(fmt.Sprintf("This is line %d\n", i))
+		if err != nil {
+			return err
+		}
+	}
+	// 必须调用 Flush 来确保缓冲区的内容被写入文件
+	return writer.Flush()
+}
+
+func main() {
+	// 记录不使用buffer的写入时间
+	start := time.Now()
+	err := writeWithoutBuffer("no_buffer.txt")
+	if err != nil {
+		fmt.Println("Error writing without buffer:", err)
+		return
+	}
+	fmt.Printf("Time taken to write without buffer: %v\n", time.Since(start))
+
+	// 记录使用buffer的写入时间
+	start = time.Now()
+	err = writeWithBuffer("with_buffer.txt")
+	if err != nil {
+		fmt.Println("Error writing with buffer:", err)
+		return
+	}
+	fmt.Printf("Time taken to write with buffer: %v\n", time.Since(start))
+}
